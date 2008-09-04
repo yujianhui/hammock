32fd19caf93005445785897a34c01e4cbdd2b469
Ben Hoskings <ben@hoskings.net>
2008-09-05 01:50:24 +1000

    Added returning_login_path.

--- lib/hammock/restful_support.rb
+++ lib/hammock/restful_support.rb
@@ -71,6 +71,12 @@ module Hammock
       def development?
         'development' == ENV['RAILS_ENV']
       end
+      
+      # TODO move to authentication when that's included in hammock.
+      def returning_login_path
+        session[:redirect_to_after_login] = request.request_uri
+        login_path
+      end
 
     end
   end
