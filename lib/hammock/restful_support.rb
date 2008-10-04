c39e98ace0fd0828a1827f876659d159fca2fc1f
Ben Hoskings <ben@hoskings.net>
2008-10-04 23:13:53 +1000

    Added assign_nestable_resources and appropriate call.

--- lib/hammock/restful_support.rb
+++ lib/hammock/restful_support.rb
@@ -37,13 +37,13 @@ module Hammock
       def table_name
         mdl.table_name
       end
-      
+
       def make_new_record
         assign_resource mdl.new params_for mdl.symbolize
       end
 
       def assign_resource record_or_records
-        if record_or_records.nil?
+        assignment = if record_or_records.nil?
           # Fail
         elsif record_or_records.is_a? ActiveRecord::Base
           instance_variable_set "@#{mdl_name}", (@record = record_or_records)
@@ -55,6 +55,31 @@ module Hammock
         else
           raise "Unknown record(s) type #{record_or_records.class}."
         end
+
+        if assign_nestable_resources
+          assignment
+        else
+          escort :not_found
+        end
+      end
+
+      def assign_nestable_resources
+        nestable_resources = self.class.nestable_resources
+        params.symbolize_keys.dragnet(*nestable_resources.keys).all? {|param_name,column_name|
+          constant = Object.const_get param_name.to_s.sub(/_id$/, '').camelize rescue nil
+
+          if constant.nil?
+            log "'#{param_name.sub(/_id$/, '').camelize}' is not available for #{param_name}."
+          elsif (record = constant.find_by_id(params[param_name])).nil?
+            log "Tab<#{params[param_name]}> not found."
+          else
+            @current_nested_records ||= []
+            @current_nested_records << record
+            @record.send "#{nestable_resources[param_name]}=", params[param_name] unless @record.nil?
+            log "Assigning @#{constant.name.underscore} with #{record.inspect}."
+            instance_variable_set "@#{constant.name.underscore}", record
+          end
+        }
       end
 
       def safe_action_and_implication? action = nil
