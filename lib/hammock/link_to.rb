4608914ca4b850af59c57b02da24156f7c785114
Ben Hoskings <ben@hoskings.net>
2008-10-04 20:42:04 +1000

    Changed outdated method name to can_verb_record?.

--- lib/hammock/link_to.rb
+++ lib/hammock/link_to.rb
@@ -12,7 +12,7 @@ module Hammock
 
     module InstanceMethods
       def link_to_if_allowed verb, record, opts = {}
-        if :ok == current_account_can_verb_record?(verb, record)
+        if :ok == can_verb_record?(verb, record)
           link_to opts[:text] || verb.to_s.capitalize, path_for(verb, record, opts), :method => method_for(verb, record)
         end
       end
