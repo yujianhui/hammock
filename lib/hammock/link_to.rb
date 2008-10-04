c7f1fd500efee5abf74880b5d9a7bd281fae960b
Ben Hoskings <ben@hoskings.net>
2008-10-04 22:21:01 +1000

    link_to_if_allowed now accepts and passes through multiple resources.

--- lib/hammock/link_to.rb
+++ lib/hammock/link_to.rb
@@ -11,8 +11,11 @@ module Hammock
     end
 
     module InstanceMethods
-      def link_to_if_allowed verb, record_or_resource, opts = {}
-        allowed = if record_or_resource.is_a?(ActiveRecord::Base)
+      def link_to_if_allowed verb, *resources
+        opts = resources.last.is_a?(Hash) ? resources.pop.symbolize_keys! : {}
+        record_or_resource = resources.last
+
+        allowed = if resources.last.is_a?(ActiveRecord::Base)
           can_verb_record? verb, record_or_resource
         else
           can_verb_resource? verb, record_or_resource
@@ -20,7 +23,7 @@ module Hammock
 
         if :ok == allowed
           link_to opts.delete(:text) || verb.to_s.capitalize,
-            path_for(verb, record_or_resource, opts),
+            path_for(verb, resources, opts),
             :method => method_for(verb, record_or_resource)
         end
       end
