3bf9af4810b061785ff1be4267ed7d81e2baaaa5
Ben Hoskings <ben@hoskings.net>
2008-10-05 12:38:42 +1000

    Pulled out scoping functionality to scope.rb from resource_retrieval.rb.

--- lib/hammock/resource_retrieval.rb
+++ lib/hammock/resource_retrieval.rb
@@ -14,32 +14,6 @@ module Hammock
 
     module InstanceMethods
 
-      def can_verb_resource? verb, resource
-        if !resource.indexable_by(@current_account)
-          log "#{requester_name} can't index #{resource.name.pluralize}."
-          :not_found
-        elsif !safe_action_and_implication?(verb) && !resource.createable_by(@current_account)
-          log "#{requester_name} can't #{verb} #{resource.name.pluralize}."
-          :read_only
-        else
-          log "#{requester_name} can #{verb} #{resource.name.pluralize}."
-          :ok
-        end
-      end
-
-      def can_verb_record? verb, record
-        if !record.readable_by?(@current_account)
-          log "#{requester_name} can't see #{record.class}<#{record.id}>."
-          :not_found
-        elsif !safe_action_and_implication?(verb) && !record.writeable_by?(@current_account)
-          log "#{requester_name} can't #{verb} #{record.class}<#{record.id}>."
-          :read_only
-        else
-          log "#{requester_name} can #{verb} #{record.class}<#{record.id}>."
-          :ok
-        end
-      end
-
       def find_record opts = {}
         result = if !callback(:before_find)
           # callbacks failed
@@ -59,31 +33,6 @@ module Hammock
         else
           escort result
         end
-      end
-
-      def verb_scope
-        if @current_account && (scope_name = account_verb_scope?)
-          log "got an account_verb_scope #{scope_name}."
-          mdl.send(scope_name, @current_account)
-        elsif !(scope_name = public_verb_scope?)
-          log "No #{@current_account.nil? ? 'public' : 'account'} #{verb_scope_name} scope available for #{mdl}.#{' May be available after login.' if account_verb_scope?}"
-          nil
-        else
-          log "got a #{scope_name} public_verb_scope."
-          mdl.send scope_name
-        end
-      end
-
-      def nest_scope
-        nestable_resources = self.class.nestable_resources
-        params.symbolize_keys.dragnet(*nestable_resources.keys).inject(mdl.ambition_context) {|acc,(k,v)|
-          # TODO this would be more ductile if it used AR assocs instead of explicit FK
-          eval "acc.select {|r| r.#{nestable_resources[k]} == v }"
-        }
-      end
-      
-      def current_scope
-        nest_scope.chain verb_scope
       end
 
       def retrieve_resource
@@ -128,6 +77,7 @@ module Hammock
         nil
       end
 
+
       private
 
       def returning_login_path
@@ -135,28 +85,6 @@ module Hammock
         login_path
       end
 
-      def verb_scope_name
-        if 'index' == action_name
-          'indexable'
-        elsif safe_action_and_implication?
-          'readable'
-        else
-          'writeable'
-        end
-      end
-      
-      def requester_name
-        @current_account.nil? ? 'Anonymous' : "#{@current_account.class}<#{@current_account.id}>"
-      end
-
-      def account_verb_scope?
-        able = "#{verb_scope_name}_by"
-        able if mdl.respond_to?(able)
-      end
-      def public_verb_scope?
-        able = verb_scope_name
-        able if mdl.respond_to?(able)
-      end
       def escort_for_bad_request
         log
         render :nothing => true, :status => 400
---
+++ lib/hammock/scope.rb
@@ -0,0 +1,96 @@
+module Hammock
+  module Scope
+    def self.included base
+      base.send :include, InstanceMethods
+      base.send :extend, ClassMethods
+
+      base.class_eval {
+        helper_method :can_verb_resource?, :can_verb_record?
+      }
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+
+      def can_verb_resource? verb, resource
+        if !resource.indexable_by(@current_account)
+          log "#{requester_name} can't index #{resource.name.pluralize}."
+          :not_found
+        elsif !safe_action_and_implication?(verb) && !resource.createable_by(@current_account)
+          log "#{requester_name} can't #{verb} #{resource.name.pluralize}."
+          :read_only
+        else
+          log "#{requester_name} can #{verb} #{resource.name.pluralize}."
+          :ok
+        end
+      end
+
+      def can_verb_record? verb, record
+        if !record.readable_by?(@current_account)
+          log "#{requester_name} can't see #{record.class}<#{record.id}>."
+          :not_found
+        elsif !safe_action_and_implication?(verb) && !record.writeable_by?(@current_account)
+          log "#{requester_name} can't #{verb} #{record.class}<#{record.id}>."
+          :read_only
+        else
+          log "#{requester_name} can #{verb} #{record.class}<#{record.id}>."
+          :ok
+        end
+      end
+
+      def verb_scope
+        if @current_account && (scope_name = account_verb_scope?)
+          log "got an account_verb_scope #{scope_name}."
+          mdl.send scope_name, @current_account
+        elsif !(scope_name = public_verb_scope?)
+          log "No #{@current_account.nil? ? 'public' : 'account'} #{verb_scope_name} scope available for #{mdl}.#{' May be available after login.' if account_verb_scope?}"
+          nil
+        else
+          log "got a #{scope_name} public_verb_scope."
+          mdl.send scope_name
+        end
+      end
+
+      def nest_scope
+        nestable_resources = self.class.nestable_resources
+        params.symbolize_keys.dragnet(*nestable_resources.keys).inject(mdl.ambition_context) {|acc,(k,v)|
+          # TODO this would be more ductile if it used AR assocs instead of explicit FK
+          eval "acc.select {|r| r.#{nestable_resources[k]} == v }"
+        }
+      end
+
+      def current_scope
+        nest_scope.chain verb_scope
+      end
+
+
+      private
+
+      def verb_scope_name
+        if 'index' == action_name
+          'indexable'
+        elsif safe_action_and_implication?
+          'readable'
+        else
+          'writeable'
+        end
+      end
+
+      def requester_name
+        @current_account.nil? ? 'Anonymous' : "#{@current_account.class}<#{@current_account.id}>"
+      end
+
+      def account_verb_scope?
+        able = "#{verb_scope_name}_by"
+        able if mdl.respond_to?(able)
+      end
+      def public_verb_scope?
+        able = verb_scope_name
+        able if mdl.respond_to?(able)
+      end
+
+    end
+  end
+end
