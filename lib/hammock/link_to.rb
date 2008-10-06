a48afe0ac82f8e3281e4a504b921040808cf47c4
Ben Hoskings <ben@hoskings.net>
2008-10-06 11:03:13 +1000

    Updated link_to_if_allowed to use can_verb_entity? and pass opts to link_to.

--- lib/hammock/link_to.rb
+++ lib/hammock/link_to.rb
@@ -16,16 +16,10 @@ module Hammock
         opts = resources.last.is_a?(Hash) ? resources.pop.symbolize_keys! : {}
         record_or_resource = resources.last
 
-        allowed = if resources.last.is_a?(ActiveRecord::Base)
-          can_verb_record? verb, record_or_resource
-        else
-          can_verb_resource? verb, record_or_resource
-        end
-
-        if :ok == allowed
+        if :ok == can_verb_entity?(verb, record_or_resource)
           link_to opts.delete(:text) || verb.to_s.capitalize,
-            path_for(verb, resources, opts),
-            :method => method_for(verb, record_or_resource)
+            path_for(verb, resources),
+            opts.merge(:method => (opts.delete(:method) || method_for(verb, record_or_resource)))
         end
       end
 
