7c898fea725bb313297f0aa9bd9e66b9b0f43702
Ben Hoskings <ben@hoskings.net>
2008-10-11 17:19:07 +1000

    Added nodoc to template self.included.

--- misc/template.rb
+++ misc/template.rb
@@ -2,7 +2,7 @@ module Hammock
   module Lol
     MixInto = Lol::Lul
     
-    def self.included base
+    def self.included base # :nodoc:
       base.send :include, InstanceMethods
       base.send :extend, ClassMethods
     end
