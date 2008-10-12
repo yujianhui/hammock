229bd6d1b7cf8344de1ee854fb0ffe232aad1580
Ben Hoskings <ben@hoskings.net>
2008-10-12 16:33:01 +1000

    Draft of monkey_patches/object docs.

--- lib/hammock/monkey_patches/object.rb
+++ lib/hammock/monkey_patches/object.rb
@@ -16,16 +16,31 @@ module Hammock
 
     module InstanceMethods
 
+      # Return +self+ after yielding to the given block.
+      #
+      # Useful for inline logging and diagnostics. Consider the following:
+      #     @items.map {|i| process(i) }.join(", ")
+      # With +tap+, adding intermediate logging is simple:
+      #     @items.map {|i| process(i) }.tap {|obj| log obj.inspect }.join(", ")
+      #--
       # TODO Remove for Ruby 1.9
       def tap
         yield self
         self
       end
 
+      # A symbolized, underscored (i.e. reverse-camelized) representation of +self+.
+      #
+      # Examples:
+      #
+      #     Hash.symbolize #=> :hash
+      #     ActiveRecord::Base.symbolize #=> :"active_record/base"
+      #     "GetThisCamelOffMyCase".symbolize #=> :get_this_camel_off_my_case
       def symbolize
         self.to_s.underscore.to_sym
       end
 
+      # If +condition+ evaluates to true, return the result of sending +method_name+, +*args+ to +self+, otherwise, return +self+ as-is.
       def send_if condition, method_name, *args
         condition ? self.send(method_name, *args) : self
       end
