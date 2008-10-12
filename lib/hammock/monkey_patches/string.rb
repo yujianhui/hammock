73cf2b73d4832d108baa9a07941aaef87b14a119
Ben Hoskings <ben@hoskings.net>
2008-10-12 16:33:19 +1000

    Draft of monkey_patches/string docs.

--- lib/hammock/monkey_patches/string.rb
+++ lib/hammock/monkey_patches/string.rb
@@ -9,33 +9,41 @@ module Hammock
 
     module ClassMethods
 
-      # Generates a random string consisting of hexadecimal characters (i.e. [0-9a-f]).
-      def af09(length = 1)
-        (1..length).inject('') {|a, t| a << rand(16).to_s(16) }
+      # Generates a random string consisting of +length+ hexadecimal characters (i.e. matching [0-9a-f]{length}).
+      def af09 length = 1
+        (1..length).inject('') {|a, t|
+          a << rand(16).to_s(16)
+        }
       end
 
-      # Generates a random string consisting of characters from [0-9a-zA-Z].
-      def azAZ09(length = 1)
-        (1..length).inject('') {|a, t| a << ((r = rand(62)) < 36 ? r.to_s(36) : (r - 26).to_s(36).upcase) }
+      # Generates a random string consisting of +length+ alphamuneric characters (i.e. matching [0-9a-zA-Z]{length}).
+      def azAZ09 length = 1
+        (1..length).inject('') {|a, t|
+          a << ((r = rand(62)) < 36 ? r.to_s(36) : (r - 26).to_s(36).upcase)
+        }
       end
 
     end
 
     module InstanceMethods
 
-      def starts_with?(str)
+      # Returns true iff +str+ appears exactly at the start of +self+.
+      def starts_with? str
         self[0, str.length] == str
       end
 
-      def ends_with?(str)
+      # Returns true iff +str+ appears exactly at the end of +self+.
+      def ends_with? str
         self[-str.length, str.length] == str
       end
 
-      def start_with(str)
+      # Return a duplicate of +self+, with +str+ prepended to it if it doesn't already start with +str+.
+      def start_with str
         starts_with?(str) ? self : str + self
       end
 
-      def end_with(str)
+      # Return a duplicate of +self+, with +str+ appended to it if it doesn't already end with +str+.
+      def end_with str
         ends_with?(str) ? self : self + str
       end
 
