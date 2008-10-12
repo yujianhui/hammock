12101a11090132d3f6625dfd927256390d6f019b
Ben Hoskings <ben@hoskings.net>
2008-10-12 21:45:42 +1000

    Removed unused get_binding.

--- lib/hammock/utils.rb
+++ lib/hammock/utils.rb
@@ -15,10 +15,6 @@ module Hammock
 
     module InstanceMethods
 
-      def get_binding
-        binding
-      end
-
       def partial_exists? name, extension = nil
         !Dir.glob(File.join(RAILS_ROOT, 'app/views', controller_name, "_#{name}.html.#{extension || '*'}")).empty?
       end
