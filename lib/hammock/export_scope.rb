f3280575363d0dc6d5a7b09e63f97ed1e74962de
Ben Hoskings <ben@hoskings.net>
2008-10-18 01:25:42 +1000

    Added has_account_scope? and has_public_scope?.

--- lib/hammock/export_scope.rb
+++ lib/hammock/export_scope.rb
@@ -9,6 +9,14 @@ module Hammock
 
     module ClassMethods
 
+      def has_public_scope? scope_name
+        "#{scope_name}able" if respond_to? "#{scope_name}_scope"
+      end
+
+      def has_account_scope? scope_name
+        "#{scope_name}able_by" if respond_to? "#{scope_name}_scope_for"
+      end
+
       def export_scopes *verbs
         verbs.each {|verb| export_scope verb }
       end
