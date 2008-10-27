bea5770e2b69ef01bdd3426ed757a7b3330b9809
Ben Hoskings <ben@hoskings.net>
2008-10-27 18:17:32 +1000

    Added has_callbacks_for?, required_callback and callback_chain_for, and removed unnecessary assignment.

--- lib/hammock/callbacks.rb
+++ lib/hammock/callbacks.rb
@@ -36,14 +36,24 @@ module Hammock
       CallbackFail = false.freeze
 
       def callback kind, *args
-        chain = self.class.send "#{kind}_callback_chain"
-
-        chain.all? {|cb|
+        callback_chain_for(kind).all? {|cb|
           # dlog "Calling #{kind} callback #{cb.method}"
           result = cb.call(self, *args) != false
           log "#{self.class}.#{cb.kind} callback '#{cb.method}' failed." unless result
           result
         }
+      end
+
+      def required_callback kind, *args
+        callback(kind, *args) if has_callbacks_for?(kind)
+      end
+
+      def has_callbacks_for? kind
+        !callback_chain_for(kind).empty?
+      end
+
+      def callback_chain_for kind
+        self.class.send "#{kind}_callback_chain"
       end
 
     end
