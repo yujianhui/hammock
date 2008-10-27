b61280746323b613eb96b41a1780864a4b848bd8
Ben Hoskings <ben@hoskings.net>
2008-10-27 18:22:03 +1000

    Added public and creator canned scopes, callable with *_resource and *_scope_for from controllers.

---
+++ lib/hammock/canned_scopes.rb
@@ -0,0 +1,56 @@
+module Hammock
+  module CannedScopes
+    MixInto = ActiveRecord::Base
+    # TODO Put this somewhere better.
+    StandardVerbs = [:read, :write, :index]
+
+    def self.included base
+      base.send :include, InstanceMethods
+      base.send :extend, ClassMethods
+    end
+
+    module ClassMethods
+
+      protected
+
+      def public_resource
+        public_scope_for *StandardVerbs
+      end
+
+      def creator_resource
+        creator_scope_for *StandardVerbs
+      end
+
+      def creator_scope_for *verbs
+        verbs = StandardVerbs if verbs.blank?
+        metaclass.instance_eval {
+          verbs.each {|verb|
+            puts "defining #{verb}_scope on #{self}"
+            send :define_method, "#{verb}_scope_for" do |account|
+              lambda {|record| record.creator_id == account.id }
+            end
+          }
+        }
+        export_scopes *verbs
+      end
+
+      def public_scope_for *verbs
+        verbs = StandardVerbs if verbs.blank?
+        metaclass.instance_eval {
+          verbs.each {|verb|
+            puts "defining #{verb}_scope on #{self}"
+            send :define_method, "#{verb}_scope" do
+              lambda {|record| true }
+            end
+          }
+        }
+        export_scopes *verbs
+      end
+
+    end
+
+    module InstanceMethods
+
+    end
+  end
+end
