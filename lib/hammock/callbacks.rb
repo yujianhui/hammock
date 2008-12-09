beb1bb8ca64e4a377a153e7ef34fd9722d3aafb8
Ben Hoskings <ben@hoskings.net>
2008-12-10 00:05:05 +1000

    Experimental new callback syntax.

--- lib/hammock/callbacks.rb
+++ lib/hammock/callbacks.rb
@@ -9,7 +9,7 @@ module Hammock
       base.class_eval {
         include ActiveSupport::Callbacks
 
-        define_callbacks *%w{
+        define_hammock_callbacks *%w{
           before_find      during_find     after_failed_find
 
           before_index     before_show
@@ -27,6 +27,53 @@ module Hammock
     end
 
     module ClassMethods
+
+      class HammockCallback < ActiveSupport::Callbacks::Callback
+        private
+
+        def evaluate_method method, *args, &block
+          if method.is_a? Proc
+            puts "was a HammockCallback proc within #{args.first.class}."
+            method.bind(args.shift).call(*args, &block)
+          else
+            super
+          end
+        end
+      end
+
+      def define_hammock_callbacks *callbacks
+        callbacks.each do |callback|
+          class_eval <<-"end_eval"
+            def self.#{callback}(*methods, &block)
+              callbacks = if !block_given? || methods.length > 1
+                CallbackChain.build(:#{callback}, *methods, &block)
+              else # hammock-style callback
+                if methods.empty?
+                  log "<-- you really should give this callback a description", :skip => 1
+                elsif !methods.first.is_a?(String)
+                  raise ArgumentError, "Inline callback definitions require a description as their sole argument."
+                else
+                  logger.info "defining \#{methods.first} on \#{name} with method \#{block.inspect}."
+                  [HammockCallback.new(:#{callback}, block, :identifier => methods.first)]
+                end || []
+              end
+              # log callbacks
+              (@#{callback}_callbacks ||= CallbackChain.new).concat callbacks
+            end
+
+            def self.#{callback}_callback_chain
+              @#{callback}_callbacks ||= CallbackChain.new
+
+              if superclass.respond_to?(:#{callback}_callback_chain)
+                CallbackChain.new(superclass.#{callback}_callback_chain + @#{callback}_callbacks)
+              else
+                @#{callback}_callbacks
+              end
+            end
+          end_eval
+        end
+      end
+
     end
 
     module InstanceMethods
