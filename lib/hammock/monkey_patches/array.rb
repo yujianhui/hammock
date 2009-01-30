b21a73a9101b26a89c2811310961e66dc189aba0
Ben Hoskings <ben@hoskings.net>
2009-01-30 17:34:31 +1000

    Moved Enumerable patches to Array.

--- lib/hammock/monkey_patches/array.rb
+++ lib/hammock/monkey_patches/array.rb
@@ -12,6 +12,16 @@ module Hammock
 
     module InstanceMethods
 
+      # Returns true iff +other+ appears exactly at the start of +self+.
+      def starts_with? *other
+        self[0, other.length] == other
+      end
+
+      # Returns true iff +other+ appears exactly at the end of +self+.
+      def ends_with? *other
+        self[-other.length, other.length] == other
+      end
+
       def squash
         self.dup.squash!
       end
