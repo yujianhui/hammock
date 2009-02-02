512e4a344fccf19037eec53f9ac1ec3902f41369
Ben Hoskings <ben@hoskings.net>
2009-02-02 21:55:36 +1000

    Removed path_for implementation in favour of the hammock route_map to generate routes through route_for (and a path_for helper).

---
+++ lib/hammock/route_for.rb
@@ -0,0 +1,54 @@
+module Hammock
+  module RouteFor
+    def self.included base # :nodoc:
+      base.send :include, InstanceMethods
+      base.send :extend, ClassMethods
+
+      base.class_eval {
+        helper_method :path_for, :route_for, :nested_route_for
+      }
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      private
+
+      def path_for *args
+        route_for(*args).path
+      end
+
+      def route_for *args
+        opts = args.extract_options!
+        verb = args.shift if args.first.is_a?(Symbol)
+
+        ActionController::Routing::Routes.route_map.for verb_for(verb, args.last), args, opts
+      end
+
+      def verb_for requested_verb, record
+        requested_verb = :show if requested_verb.blank?
+        
+        if (:show == requested_verb) && record.is_a?(Class)
+          :index
+        elsif :modify == requested_verb
+          record.new_record? ? :new : :edit
+        elsif :save == requested_verb
+          record.new_record? ? :create : :update
+        else
+          requested_verb
+        end
+      end
+
+      def nested_route_for *resources
+        resources.delete_if &:nil?
+        requested_verb = resources.shift if resources.first.is_a?(Symbol)
+        args = @current_nested_records.dup.concat(resources)
+
+        args.unshift(requested_verb) unless requested_verb.nil?
+        route_for *args
+      end
+
+    end
+  end
+end
