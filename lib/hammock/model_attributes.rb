86606aa21a46e573c1e1a149f74dc5d5cd4a72a2
Ben Hoskings <ben@hoskings.net>
2009-02-25 17:52:04 +1000

    Added AR::Base#route_by (setting to_param and replacing AC::Base#find_column) and #nest_within, along with corresponding attribute readers.

--- lib/hammock/model_attributes.rb
+++ lib/hammock/model_attributes.rb
@@ -9,6 +9,15 @@ module Hammock
 
     module ClassMethods
 
+      def route_by attribute
+        write_inheritable_attribute "routing_attribute", attribute.to_sym
+        define_method :to_param do
+          send self.class.routing_attribute
+        end
+      end
+      def nest_within *attributes
+        write_inheritable_attribute "nestable_routing_resources", attributes
+      end
       def has_defaults attrs
         write_inheritable_attribute "default_attributes", (default_attributes || {}).merge(attrs)
       end
@@ -19,6 +28,12 @@ module Hammock
         write_inheritable_attribute "attr_accessible_on_update", Set.new(attributes.map(&:to_s)) + (accessible_attributes_on_update || [])
       end
 
+      def routing_attribute
+        read_inheritable_attribute("routing_attribute")
+      end
+      def nestable_routing_resources
+        read_inheritable_attribute("nestable_routing_resources")
+      end
       def default_attributes
         read_inheritable_attribute("default_attributes") || {}
       end
