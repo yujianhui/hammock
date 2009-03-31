6aeb12dbcbaff3f3e99993e21a478c131e171afd
Ben Hoskings <ben@hoskings.net>
2009-03-31 15:28:03 +1000

    Bumped version to 0.2.12.

--- lib/hammock.rb
+++ lib/hammock.rb
@@ -4,7 +4,7 @@ require 'ambition'
 require 'ambition/adapters/active_record'
 
 module Hammock
-  VERSION = '0.2.11.4'
+  VERSION = '0.2.12'
 
   def self.included base # :nodoc:
     puts "Loading Hammock from #{loaded_from_gem? ? 'gem' : 'plugin'}"
